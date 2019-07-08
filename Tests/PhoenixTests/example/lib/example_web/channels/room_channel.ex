d6928d12138bbf5ac61f3a005b2b4e2e2ad8ccfa
Nathan Herald <me@nathanherald.com>
2019-07-09 01:01:24 +0200

    Add some extra APIs for testing channel behavior

--- Tests/PhoenixTests/example/lib/example_web/channels/room_channel.ex
+++ Tests/PhoenixTests/example/lib/example_web/channels/room_channel.ex
@@ -17,6 +17,14 @@ defmodule ExampleWeb.RoomChannel do
     {:noreply, socket}
   end
 
+  def handle_in("echo", %{"echo" => echo_text}, socket) do
+    {:reply, {:ok, %{echo: echo_text}}, socket}
+  end
+
+  def handle_in("echo_error", %{"error" => echo_text}, socket) do
+    {:reply, {:error, %{error: echo_text}}, socket}
+  end
+
   def handle_in("repeat", %{"echo" => echo_text, "amount" => amount}, socket)
       when is_integer(amount) do
     for n <- 1..amount do
