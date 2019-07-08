534ab156c394bac611b00d4e7180d6341622a386
Nathan Herald <me@nathanherald.com>
2019-07-09 01:02:23 +0200

    Add some amazing tests

--- Tests/PhoenixTests/ChannelTests.swift
+++ Tests/PhoenixTests/ChannelTests.swift
@@ -47,4 +47,141 @@ class ChannelTests: XCTestCase {
         
         wait(for: [channelLeftEx], timeout: 0.25)
     }
+    
+    func testPushCallback() throws {
+        let socket = try Socket(url: helper.deafultURL)
+        helper.wait { socket.isOpen }
+        XCTAssert(socket.isOpen, "Socket should have been open")
+        
+        let channelJoinedEx = XCTestExpectation(description: "Channel joined")
+        
+        let channel = socket.join("room:lobby")
+        
+        let _ = channel.forever { result in
+            if case .success(.join) = result {
+                return channelJoinedEx.fulfill()
+            }
+        }
+        
+        wait(for: [channelJoinedEx], timeout: 0.25)
+        
+        let repliedOKEx = XCTestExpectation(description: "Received OK reply")
+        let repliedErrorEx = XCTestExpectation(description: "Received errro reply")
+        
+        channel.push("echo", payload: ["echo": "hello"]) { reply in
+            XCTAssert(reply.isOk, "Reply should have been OK")
+            
+            let echo = reply.response["echo"] as? String
+            
+            XCTAssertEqual(echo, "hello")
+            
+            repliedOKEx.fulfill()
+        }
+        
+        channel.push("echo_error", payload: ["error": "whatever"]) { reply in
+            XCTAssert(reply.isNotOk, "Reply should have been not OK")
+            
+            let error = reply.response["error"] as? String
+            
+            XCTAssertEqual(error, "whatever")
+            
+            repliedErrorEx.fulfill()
+        }
+        
+        wait(for: [repliedOKEx, repliedErrorEx], timeout: 0.25)
+    }
+    
+    func testReceiveMessages() throws {
+        let socket = try Socket(url: helper.deafultURL)
+        helper.wait { socket.isOpen }
+        XCTAssert(socket.isOpen, "Socket should have been open")
+        
+        let channelJoinedEx = XCTestExpectation(description: "Channel joined")
+        let messageRepeatedEx = XCTestExpectation(description: "Message repeated correctly")
+        let echoText = "This should be repeated"
+        
+        let channel = socket.join("room:lobby")
+        var messageCounter = 0
+        
+        let _ = channel.forever { result in
+            if case .success(.join) = result {
+                return channelJoinedEx.fulfill()
+            }
+            
+            if case .success(.message(let message)) = result {
+                messageCounter += 1
+                
+                XCTAssertEqual(message.event, "repeated")
+                
+                let echo = message.payload["echo"] as? String
+                XCTAssertEqual(echo, echoText)
+                
+                if messageCounter >= 5 {
+                    messageRepeatedEx.fulfill()
+                }
+                
+                return
+            }
+        }
+        
+        wait(for: [channelJoinedEx], timeout: 0.25)
+        
+        let payload: [String: Any] = ["echo": echoText, "amount": 5]
+        
+        channel.push("repeat", payload: payload)
+        
+        wait(for: [messageRepeatedEx], timeout: 0.25)
+    }
+    
+    func testMultipleSocketsCollaborating() throws {
+        let socket1 = try Socket(url: helper.deafultURL)
+        helper.wait { socket1.isOpen }
+        XCTAssert(socket1.isOpen, "Socket should have been open")
+        
+        let socket2 = try Socket(url: helper.deafultURL)
+        helper.wait { socket2.isOpen }
+        XCTAssert(socket2.isOpen, "Socket should have been open")
+        
+        let channel1 = socket1.join("room:lobby")
+        let channel2 = socket2.join("room:lobby")
+        
+        let messageText = "This should get broadcasted 😎"
+        
+        let channel1JoinedEx = XCTestExpectation(description: "Channel 1 joined")
+        let channel2JoinedEx = XCTestExpectation(description: "Channel 2 joined")
+        let channel1ReceivedMessageEx = XCTestExpectation(description: "Channel 1 received the message")
+        let channel2ReceivedMessageEx = XCTestExpectation(description: "Channel 2 received the message which was not right")
+        channel2ReceivedMessageEx.isInverted = true
+        
+        let _ = channel1.forever { result in
+            if case .success(.join) = result {
+                return channel1JoinedEx.fulfill()
+            }
+            
+            if case .success(.message(let message)) = result {
+                let text = message.payload["text"] as? String
+                
+                if message.event == "message" && text == messageText {
+                    channel1ReceivedMessageEx.fulfill()
+                }
+                return
+            }
+        }
+        
+        let _ = channel2.forever { result in
+            if case .success(.join) = result {
+                return channel2JoinedEx.fulfill()
+            }
+            
+            if case .success(.message(_)) = result {
+                channel2ReceivedMessageEx.fulfill()
+            }
+        }
+        
+        wait(for: [channel1JoinedEx, channel2JoinedEx], timeout: 0.25)
+        
+        channel2.push("insert_message", payload: ["text": messageText])
+        
+        wait(for: [channel1ReceivedMessageEx], timeout: 0.25)
+    }
 }
