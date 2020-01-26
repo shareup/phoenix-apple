3e1e5989bb45d24c9861f2b59b4d570e481596be
Nathan Herald <me@nathanherald.com>
2020-01-26 17:49:22 +0100

    Build a quick javascript socket page for testing

--- Tests/PhoenixTests/example/lib/example_web/endpoint.ex
+++ Tests/PhoenixTests/example/lib/example_web/endpoint.ex
@@ -2,4 +2,6 @@ defmodule ExampleWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :example
 
   socket "/socket", ExampleWeb.Socket, websocket: true, longpoll: false
+
+  plug Plug.Static, at: "/", from: :example
 end
