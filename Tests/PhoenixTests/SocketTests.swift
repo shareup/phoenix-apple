ab095140dca4c24636ca280f0cd49bf98fc33593
Anthony Drendel <me@anthonydrendel.com>
2020-06-06 18:46:13 +0200

    Add more test helpers to SocketTests

--- Tests/PhoenixTests/SocketTests.swift
+++ Tests/PhoenixTests/SocketTests.swift
@@ -653,27 +653,16 @@ class SocketTests: XCTestCase {
         defer { socket.disconnect() }
         
         let channel = socket.join("room:lobby")
-        
-        let joinedEx = expectation(description: "Channel should have joined")
-        let erroredEx = expectation(description: "Channel should have errored")
-        
-        let sub = channel.forever { result in
-            switch result {
-            case .join:
-                joinedEx.fulfill()
-            case .error:
-                erroredEx.fulfill()
-            default:
-                break
-            }
-        }
+
+        let sub = channel.forever(receiveValue:
+            awaitAndThen([
+                .join: { socket.send("disconnect") },
+                .error: { }
+            ])
+        )
         defer { sub.cancel() }
         
         socket.connect()
-        
-        wait(for: [joinedEx], timeout: 0.3)
-        
-        socket.send("disconnect")
         
         waitForExpectations(timeout: 1)
     }
@@ -686,25 +675,17 @@ class SocketTests: XCTestCase {
         
         let joinedEx = expectation(description: "Channel should have joined")
         let erroredEx = expectation(description: "Channel should have errored")
-        
-        let sub = channel.forever { result in
-            switch result {
-            case .join:
-                joinedEx.fulfill()
-            case .error:
-                erroredEx.fulfill()
-            default:
-                break
-            }
-        }
+
+        let sub = channel.forever(receiveValue:
+            onResults([
+                .join: { joinedEx.fulfill(); socket.send("boom") },
+                .error: { erroredEx.fulfill() }
+            ])
+        )
         defer { sub.cancel() }
         
         socket.connect()
-        
-        wait(for: [joinedEx], timeout: 0.3)
-        
-        socket.send("boom")
-        
+
         waitForExpectations(timeout: 1)
     }
 
@@ -823,10 +804,35 @@ private extension SocketTests {
         wait(for: [joinedEx, leftEx], timeout: 1)
     }
 
+    func awaitAndThen<T: RawCaseConvertible>(_ valueToAction: Dictionary<T.RawCase, (() -> Void)>) -> (T) -> Void {
+        let valueToExpectation = valueToAction.reduce(into: Dictionary<T.RawCase, XCTestExpectation>())
+        { [unowned self] (dict, valueToAction) in
+            let key = valueToAction.key
+            let expectation = self.expectation(description: "Should have \(String(describing: key))")
+            dict[key] = expectation
+        }
+
+        return { v in
+            let rawCase = v.toRawCase()
+            if let block = valueToAction[rawCase], let expectation = valueToExpectation[rawCase] {
+                expectation.fulfill()
+                block()
+            }
+        }
+    }
+
     func onResult<T: RawCaseConvertible>(_ value: T.RawCase, _ block: @escaping @autoclosure () -> Void) -> (T) -> Void {
         return { v in
             guard v.matches(value) else { return }
             block()
+        }
+    }
+
+    func onResults<T: RawCaseConvertible>(_ valueToAction: Dictionary<T.RawCase, (() -> Void)>) -> (T) -> Void {
+        return { v in
+            if let block = valueToAction[v.toRawCase()] {
+                block()
+            }
         }
     }
 }
