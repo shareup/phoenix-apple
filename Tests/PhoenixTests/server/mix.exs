6352840063c537e6a95945306d1de79c90fcc999
Anthony Drendel <me@anthonydrendel.com>
2020-06-07 15:32:37 +0200

    Update server dependencies

--- Tests/PhoenixTests/server/mix.exs
+++ Tests/PhoenixTests/server/mix.exs
@@ -32,8 +32,8 @@ defmodule Server.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.12"},
-      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.5"},
+      {:phoenix_pubsub, "~> 2.0"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
     ]
