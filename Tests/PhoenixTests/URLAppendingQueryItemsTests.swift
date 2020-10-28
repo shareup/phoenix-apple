d86675a96a30e6c230c444514ea262adb9fe9108
Anthony Drendel <me@anthonydrendel.com>
2020-10-29 00:01:54 +0100

    Form-data-encode query parameters

---
+++ Tests/PhoenixTests/URLAppendingQueryItemsTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import Phoenix
+
+class URLAppendingQueryItemsTests: XCTestCase {
+    func testQueryItemsAreFormEncoded() throws {
+        let url = try XCTUnwrap(URL(string: "ws://localhost:4000/socket"))
+        let query = [
+            "shared_secret": "tfa7DS9iIX/POq9/lAiIK4UZS+FXca1uQFMqfvJdpXY8zMUGF2vWEFXtotGcIMED"
+        ]
+
+        let urlWithQueryItems = url.appendingQueryItems(query)
+        let expected = "ws://localhost:4000/socket?shared_secret=tfa7DS9iIX%2FPOq9%2FlAiIK4UZS%2BFXca1uQFMqfvJdpXY8zMUGF2vWEFXtotGcIMED"
+
+        XCTAssertEqual(expected, urlWithQueryItems.absoluteString)
+    }
+}
