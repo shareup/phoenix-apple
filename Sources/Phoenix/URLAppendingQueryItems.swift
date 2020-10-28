597acb681ea2ffc07387d8a3e32bcca77d7c23d5
Anthony Drendel <atdrendel@users.noreply.github.com>
2020-10-29 00:06:15 +0100

    Form-data-encode query parameters (#17)

--- Sources/Phoenix/URLAppendingQueryItems.swift
+++ Sources/Phoenix/URLAppendingQueryItems.swift
@@ -1,23 +1,53 @@
 import Foundation
 
+// https://github.com/phoenixframework/phoenix/blob/e82161eb8f724a69cb17b8e2a22ea54e595b29ca/lib/phoenix/transports/websocket.ex#L17
+//
+// https://hexdocs.pm/plug/Plug.Conn.html#fetch_query_params/2
+//
+// Phoenix uses Plug.Conn.fetch_query_params() to collect the query parameters in Socket.connect().
+// Plug.Conn.fetch_query_params() decodes the parameters as `x-www-form-urlencoded`. However,
+// `URLComponents` does not encode query items according to the `x-www-form-urlencoded` standard.
+// So, we have to manually encode all query parameters as `x-www-form-urlencoded`.
+
 extension URL {
     func appendingQueryItems(_ items: [String: String]) -> URL {
         guard var components = URLComponents(url: self, resolvingAgainstBaseURL: false) else {
             fatalError()
         }
         
-        var query = components.queryItems ?? []
+        var queryItems: [URLQueryItem] = components
+            .queryItemsOrEmpty
+            .map { URLQueryItem(name: $0.name, value: $0.value?.addingPercentEncodingForFormData()) }
         
         items.forEach { (name, value) in
-            query.append(URLQueryItem(name: name, value: value))
+            queryItems.append(
+                URLQueryItem(name: name, value: value.addingPercentEncodingForFormData())
+            )
         }
         
-        components.queryItems = query
-        
+        components.percentEncodedQueryItems = queryItems
+
         guard let url = components.url else {
             fatalError()
         }
         
         return url
+    }
+}
+
+extension URLComponents {
+    fileprivate var queryItemsOrEmpty: [URLQueryItem] {
+        queryItems ?? []
+    }
+}
+
+extension String {
+    fileprivate func addingPercentEncodingForFormData() -> String? {
+        let allowedCharacters = "*-._ "
+        var allowedCharacterSet = CharacterSet.alphanumerics
+        allowedCharacterSet.insert(charactersIn: allowedCharacters)
+
+        return addingPercentEncoding(withAllowedCharacters: allowedCharacterSet)?
+            .replacingOccurrences(of: " ", with: "+")
     }
 }
