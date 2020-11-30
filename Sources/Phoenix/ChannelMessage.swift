2d8353cec66b30f09d4e07513805b66a59bcd716
Anthony Drendel <me@anthonydrendel.com>
2020-11-30 23:22:05 +0100

    Create public memberwise initializer for Channel.Message

--- Sources/Phoenix/ChannelMessage.swift
+++ Sources/Phoenix/ChannelMessage.swift
@@ -2,6 +2,11 @@ extension Channel {
     public struct Message {
         public let event: String
         public let payload: Payload
+
+        public init(event: String, payload: Payload) {
+            self.event = event
+            self.payload = payload
+        }
         
         init(incomingMessage: IncomingMessage) {
             self.event = incomingMessage.event.stringValue
