fb978f81b6ae1701f949b1408dfcbfbc9fa0d065
Anthony Drendel <atdrendel@users.noreply.github.com>
2021-01-08 15:36:48 +0100

    Change test server port to 4003 (#29)

--- Tests/PhoenixTests/URLAppendingQueryItemsTests.swift
+++ Tests/PhoenixTests/URLAppendingQueryItemsTests.swift
@@ -3,13 +3,13 @@ import XCTest
 
 class URLAppendingQueryItemsTests: XCTestCase {
     func testQueryItemsAreFormEncoded() throws {
-        let url = try XCTUnwrap(URL(string: "ws://localhost:4000/socket"))
+        let url = try XCTUnwrap(URL(string: "ws://localhost:4003/socket"))
         let query = [
             "shared_secret": "tfa7DS9iIX/POq9/lAiIK4UZS+FXca1uQFMqfvJdpXY8zMUGF2vWEFXtotGcIMED"
         ]
 
         let urlWithQueryItems = url.appendingQueryItems(query)
-        let expected = "ws://localhost:4000/socket?shared_secret=tfa7DS9iIX%2FPOq9%2FlAiIK4UZS%2BFXca1uQFMqfvJdpXY8zMUGF2vWEFXtotGcIMED"
+        let expected = "ws://localhost:4003/socket?shared_secret=tfa7DS9iIX%2FPOq9%2FlAiIK4UZS%2BFXca1uQFMqfvJdpXY8zMUGF2vWEFXtotGcIMED"
 
         XCTAssertEqual(expected, urlWithQueryItems.absoluteString)
     }
