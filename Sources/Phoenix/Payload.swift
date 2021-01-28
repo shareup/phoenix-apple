a9f3b6e4b8df2cdfc798ba6d8e3fceb5e0b5a0e5
Anthony Drendel <me@anthonydrendel.com>
2021-01-28 10:00:03 +0100

    Limit Payload _debugDescription

--- Sources/Phoenix/Payload.swift
+++ Sources/Phoenix/Payload.swift
@@ -5,7 +5,14 @@ public typealias Payload = [String: Any]
 extension Dictionary where Key == String, Value == Any {
     var _debugDescription: String {
         let contents = self
-            .map { "\($0.key):\(String(describing: $0.value))" }
+            .map { (key, value) in
+                let maxLength = 32
+                let valueStr = String(describing: value)
+                let truncatedValue = valueStr.count > maxLength ?
+                    "\(valueStr.prefix(maxLength))..." :
+                    valueStr
+                return "\(key):\(truncatedValue)"
+            }
             .joined(separator: ",")
         return "{\(contents)}"
     }
